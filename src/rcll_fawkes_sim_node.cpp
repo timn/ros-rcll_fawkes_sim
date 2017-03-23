d882cfbb634478b3051d1e7d0a9ebf839588da35
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2017-03-23 11:17:07 +0100

    rcll_fawkes_sim: fix initialization race condition
    The initialization could succeed in establishing a blackboard
    connection, but then fail to establish all required interfaces. This
    would leave the node defunct.
    
    Along the way move from module to class code.

--- src/rcll_fawkes_sim_node.cpp
+++ src/rcll_fawkes_sim_node.cpp
@@ -48,26 +48,6 @@
 		} \
 	}
 
-std::string  cfg_fawkes_host_;
-int          cfg_fawkes_port_;
-
-std::shared_ptr<fawkes::BlackBoard> blackboard_;
-class DataPasser;
-std::shared_ptr<DataPasser> dp_;
-
-fawkes::ZoneInterface       *ifc_zone_;
-fawkes::TagVisionInterface  *ifc_tag_vision_;
-std::array<fawkes::Position3DInterface *, 16> ifcs_tag_pos_;
-fawkes::RobotinoLightInterface  *ifc_machine_signal_;
-fawkes::NavGraphWithMPSGeneratorInterface *ifc_navgraph_gen_;
-fawkes::Position3DInterface *ifc_pose_;
-
-ros::Publisher pub_expl_zone_info_;
-ros::Publisher pub_mps_marker_array_;
-ros::Publisher pub_mps_light_state_;
-ros::Publisher pub_pose_;
-ros::ServiceServer srv_navgraph_gen_;
-
 int
 fawkes_to_ros_light_state(const fawkes::RobotinoLightInterface::LightState &state)
 {
@@ -92,128 +72,74 @@ ros_to_fawkes_mps_side(int side)
 	}
 }
 
-bool
-srv_cb_navgraph_gen(rcll_fawkes_sim_msgs::NavgraphWithMPSGenerate::Request  &req,
-                    rcll_fawkes_sim_msgs::NavgraphWithMPSGenerate::Response &res)
+class RcllFawkesSimNode : public fawkes::BlackBoardInterfaceListener
 {
-	if (! blackboard_->is_alive()) {
-		res.ok = false;
-		res.error_msg = "Blackboard is not connected";
-		return true;
-	}
-	
-	if (! ifc_navgraph_gen_->has_writer()) {
-		res.ok = false;
-		res.error_msg = "No writer for navgraph generator with MPS interface";
-		return true;
-	}
+public:
+	RcllFawkesSimNode(ros::NodeHandle rn, std::shared_ptr<fawkes::BlackBoard> bb)
+		: BlackBoardInterfaceListener("RcllFawkesSimNode"),
+		  rosnode(rn), blackboard_(bb)
+	{}
 
-	std::queue<fawkes::Message *> msgs;
-	
-	ifc_navgraph_gen_->msgq_enqueue(new fawkes::NavGraphWithMPSGeneratorInterface::ClearMessage());
-
-	for (size_t i = 0; i < req.mps_stations.size(); ++i) {
-		const rcll_fawkes_sim_msgs::NavgraphMPSStation &mps = req.mps_stations[i];
-		fawkes::NavGraphWithMPSGeneratorInterface::UpdateStationByTagMessage *upm =
-			new fawkes::NavGraphWithMPSGeneratorInterface::UpdateStationByTagMessage();
-#ifdef HAVE_OLD_NAVGRAPH_GENMPS_INTERFACE
-		upm->set_id(mps.name.c_str());
-#else
-		upm->set_name(mps.name.c_str());
-#endif
-		upm->set_side(ros_to_fawkes_mps_side(mps.marker_side));
-		upm->set_frame(mps.marker_frame.c_str());
-		upm->set_tag_translation(0, mps.marker_pose.position.x);
-		upm->set_tag_translation(1, mps.marker_pose.position.y);
-		upm->set_tag_translation(2, mps.marker_pose.position.z);
-		upm->set_tag_rotation(0, mps.marker_pose.orientation.x);
-		upm->set_tag_rotation(1, mps.marker_pose.orientation.y);
-		upm->set_tag_rotation(2, mps.marker_pose.orientation.z);
-		upm->set_tag_rotation(3, mps.marker_pose.orientation.w);
-		msgs.push(upm);
-	}
-
+	void
+	init()
 	{
-		fawkes::NavGraphWithMPSGeneratorInterface::SetExplorationZonesMessage *sezm =
-			new fawkes::NavGraphWithMPSGeneratorInterface::SetExplorationZonesMessage();
-		bool zones[sezm->maxlenof_zones()];
-		for (int i = 0; i < sezm->maxlenof_zones(); ++i) zones[i] = false;
-		for (size_t i = 0; i < req.exploration_zones.size(); ++i) {
-			int zone_idx = req.exploration_zones[i] - 1;
-			if (zone_idx < 0 || zone_idx > sezm->maxlenof_zones()) {
-				delete sezm;
-				res.ok = false;
-				res.error_msg = "Exploration zone index out of bounds";
-				return true;
-			}
-			zones[zone_idx] = true;
+		ifc_zone_ = blackboard_->open_for_reading<fawkes::ZoneInterface>("/explore-zone/info");
+		ifc_tag_vision_ = blackboard_->open_for_reading<fawkes::TagVisionInterface>("/tag-vision/info");
+		for (size_t i = 0; i < std::min(ifc_tag_vision_->maxlenof_tag_id(), ifcs_tag_pos_.size()); ++i) {
+			std::string ifc_id = "/tag-vision/" + std::to_string(i);
+			ifcs_tag_pos_[i] = blackboard_->open_for_reading<fawkes::Position3DInterface>(ifc_id.c_str());
+			// do not listen for events on these interfaces, we read them
+			// whenever the tag vision info interface changes
 		}
-		sezm->set_zones(zones);
-		msgs.push(sezm);
+		ifc_machine_signal_ = blackboard_->open_for_reading<fawkes::RobotinoLightInterface>("/machine-signal/best");
+		ifc_navgraph_gen_ =
+			blackboard_->open_for_reading<fawkes::NavGraphWithMPSGeneratorInterface>("/navgraph-generator-mps");
+		ifc_pose_ = blackboard_->open_for_reading<fawkes::Position3DInterface>("Pose");
+
+		
+		bbil_add_data_interface(ifc_zone_);
+		bbil_add_data_interface(ifc_tag_vision_);
+		bbil_add_data_interface(ifc_machine_signal_);
+		bbil_add_data_interface(ifc_pose_);
+
+		// Setup ROS topics
+		pub_expl_zone_info_ =
+			rosnode.advertise<rcll_fawkes_sim_msgs::ExplorationZoneInfo>("rcll_sim/explore_zone_info", 10);
+		pub_mps_marker_array_ =
+			rosnode.advertise<rcll_fawkes_sim_msgs::MPSMarkerArray>("rcll_sim/mps_marker_array", 10);
+		pub_mps_light_state_ =
+			rosnode.advertise<rcll_fawkes_sim_msgs::MPSLightState>("rcll_sim/mps_light_state", 10);
+		pub_pose_ =
+			rosnode.advertise<geometry_msgs::PoseWithCovarianceStamped>("rcll_sim/amcl_pose", 10);
+
+		blackboard_->register_listener(this, fawkes::BlackBoard::BBIL_FLAG_DATA);
+
+		// provide services
+		srv_navgraph_gen_ = rosnode.advertiseService("rcll_sim/navgraph_generate",
+		                                             &RcllFawkesSimNode::srv_cb_navgraph_gen, this);
 	}
 
+	void
+	finalize()
 	{
-		fawkes::NavGraphWithMPSGeneratorInterface::SetWaitZonesMessage *swzm =
-			new fawkes::NavGraphWithMPSGeneratorInterface::SetWaitZonesMessage();
-		bool zones[swzm->maxlenof_zones()];
-		for (int i = 0; i < swzm->maxlenof_zones(); ++i) zones[i] = false;
-		for (size_t i = 0; i < req.wait_zones.size(); ++i) {
-			int zone_idx = req.wait_zones[i] - 1;
-			if (zone_idx < 0 || zone_idx > swzm->maxlenof_zones()) {
-				delete swzm;
-				res.ok = false;
-				res.error_msg = "Wait zone index out of bounds";
-				return true;
-			}
-			zones[zone_idx] = true;
+		blackboard_->unregister_listener(this);
+
+		blackboard_->close(ifc_zone_);
+		blackboard_->close(ifc_tag_vision_);
+		for (const auto &i : ifcs_tag_pos_) {
+			blackboard_->close(i);
 		}
-		swzm->set_zones(zones);
-		msgs.push(swzm);
+		blackboard_->close(ifc_machine_signal_);
+		blackboard_->close(ifc_navgraph_gen_);
+		blackboard_->close(ifc_pose_);
+
+		pub_expl_zone_info_.shutdown();
+		pub_mps_marker_array_.shutdown();
+		pub_mps_light_state_.shutdown();
+		pub_pose_.shutdown();
+		srv_navgraph_gen_.shutdown();
 	}
 
-	while (! msgs.empty()) {
-		fawkes::Message *m = msgs.front();
-		ifc_navgraph_gen_->msgq_enqueue(m);
-		msgs.pop();
-	}
-
-	fawkes::NavGraphWithMPSGeneratorInterface::ComputeMessage *compmsg =
-		new fawkes::NavGraphWithMPSGeneratorInterface::ComputeMessage();
-	compmsg->ref();
-	ifc_navgraph_gen_->msgq_enqueue(compmsg);
-	unsigned int msgid = compmsg->id();
-	compmsg->unref();
-	bool finished = false;
-	while (! finished && ifc_navgraph_gen_->has_writer()) {
-		ifc_navgraph_gen_->read();
-		if (ifc_navgraph_gen_->msgid() == msgid && ifc_navgraph_gen_->is_final()) {
-			res.ok = true;
-			finished = true;
-		}
-		ros::Duration(0.1 /* sec */).sleep();
-	}
-
-	if (finished) {
-		res.ok = true;
-	} else {
-		res.ok = false;
-		res.error_msg = "Navgraph generator with MPS interface writer disappeared while waiting";
-	}
-
-	return true;
-}
-
-class DataPasser : public fawkes::BlackBoardInterfaceListener
-{
- public:
-	DataPasser() : BlackBoardInterfaceListener("RCLL_ROS_DataPasser") {}
-	
-	void add(fawkes::Interface *interface)
-	{
-		ROS_INFO("%s: Adding data interface %s", ros::this_node::getName().c_str(), interface->uid());
-		bbil_add_data_interface(interface);
-	}
-	
 	virtual void bb_interface_data_changed(fawkes::Interface *interface) throw()
 	{
 		interface->read();
@@ -287,49 +213,137 @@ class DataPasser : public fawkes::BlackBoardInterfaceListener
 		}
 
 	}
+
+	bool
+	srv_cb_navgraph_gen(rcll_fawkes_sim_msgs::NavgraphWithMPSGenerate::Request  &req,
+	                    rcll_fawkes_sim_msgs::NavgraphWithMPSGenerate::Response &res)
+	{
+		if (! blackboard_->is_alive()) {
+			res.ok = false;
+			res.error_msg = "Blackboard is not connected";
+			return true;
+		}
+	
+		if (! ifc_navgraph_gen_->has_writer()) {
+			res.ok = false;
+			res.error_msg = "No writer for navgraph generator with MPS interface";
+			return true;
+		}
+
+		std::queue<fawkes::Message *> msgs;
+	
+		ifc_navgraph_gen_->msgq_enqueue(new fawkes::NavGraphWithMPSGeneratorInterface::ClearMessage());
+
+		for (size_t i = 0; i < req.mps_stations.size(); ++i) {
+			const rcll_fawkes_sim_msgs::NavgraphMPSStation &mps = req.mps_stations[i];
+			fawkes::NavGraphWithMPSGeneratorInterface::UpdateStationByTagMessage *upm =
+				new fawkes::NavGraphWithMPSGeneratorInterface::UpdateStationByTagMessage();
+#ifdef HAVE_OLD_NAVGRAPH_GENMPS_INTERFACE
+			upm->set_id(mps.name.c_str());
+#else
+			upm->set_name(mps.name.c_str());
+#endif
+			upm->set_side(ros_to_fawkes_mps_side(mps.marker_side));
+			upm->set_frame(mps.marker_frame.c_str());
+			upm->set_tag_translation(0, mps.marker_pose.position.x);
+			upm->set_tag_translation(1, mps.marker_pose.position.y);
+			upm->set_tag_translation(2, mps.marker_pose.position.z);
+			upm->set_tag_rotation(0, mps.marker_pose.orientation.x);
+			upm->set_tag_rotation(1, mps.marker_pose.orientation.y);
+			upm->set_tag_rotation(2, mps.marker_pose.orientation.z);
+			upm->set_tag_rotation(3, mps.marker_pose.orientation.w);
+			msgs.push(upm);
+		}
+
+		{
+			fawkes::NavGraphWithMPSGeneratorInterface::SetExplorationZonesMessage *sezm =
+				new fawkes::NavGraphWithMPSGeneratorInterface::SetExplorationZonesMessage();
+			bool zones[sezm->maxlenof_zones()];
+			for (int i = 0; i < sezm->maxlenof_zones(); ++i) zones[i] = false;
+			for (size_t i = 0; i < req.exploration_zones.size(); ++i) {
+				int zone_idx = req.exploration_zones[i] - 1;
+				if (zone_idx < 0 || zone_idx > sezm->maxlenof_zones()) {
+					delete sezm;
+					res.ok = false;
+					res.error_msg = "Exploration zone index out of bounds";
+					return true;
+				}
+				zones[zone_idx] = true;
+			}
+			sezm->set_zones(zones);
+			msgs.push(sezm);
+		}
+
+		{
+			fawkes::NavGraphWithMPSGeneratorInterface::SetWaitZonesMessage *swzm =
+				new fawkes::NavGraphWithMPSGeneratorInterface::SetWaitZonesMessage();
+			bool zones[swzm->maxlenof_zones()];
+			for (int i = 0; i < swzm->maxlenof_zones(); ++i) zones[i] = false;
+			for (size_t i = 0; i < req.wait_zones.size(); ++i) {
+				int zone_idx = req.wait_zones[i] - 1;
+				if (zone_idx < 0 || zone_idx > swzm->maxlenof_zones()) {
+					delete swzm;
+					res.ok = false;
+					res.error_msg = "Wait zone index out of bounds";
+					return true;
+				}
+				zones[zone_idx] = true;
+			}
+			swzm->set_zones(zones);
+			msgs.push(swzm);
+		}
+
+		while (! msgs.empty()) {
+			fawkes::Message *m = msgs.front();
+			ifc_navgraph_gen_->msgq_enqueue(m);
+			msgs.pop();
+		}
+
+		fawkes::NavGraphWithMPSGeneratorInterface::ComputeMessage *compmsg =
+			new fawkes::NavGraphWithMPSGeneratorInterface::ComputeMessage();
+		compmsg->ref();
+		ifc_navgraph_gen_->msgq_enqueue(compmsg);
+		unsigned int msgid = compmsg->id();
+		compmsg->unref();
+		bool finished = false;
+		while (! finished && ifc_navgraph_gen_->has_writer()) {
+			ifc_navgraph_gen_->read();
+			if (ifc_navgraph_gen_->msgid() == msgid && ifc_navgraph_gen_->is_final()) {
+				res.ok = true;
+				finished = true;
+			}
+			ros::Duration(0.1 /* sec */).sleep();
+		}
+
+		if (finished) {
+			res.ok = true;
+		} else {
+			res.ok = false;
+			res.error_msg = "Navgraph generator with MPS interface writer disappeared while waiting";
+		}
+
+		return true;
+	}
+
+private:	
+	ros::NodeHandle rosnode;
+
+	std::shared_ptr<fawkes::BlackBoard> blackboard_;
+
+	fawkes::ZoneInterface       *ifc_zone_;
+	fawkes::TagVisionInterface  *ifc_tag_vision_;
+	std::array<fawkes::Position3DInterface *, 16> ifcs_tag_pos_;
+	fawkes::RobotinoLightInterface  *ifc_machine_signal_;
+	fawkes::NavGraphWithMPSGeneratorInterface *ifc_navgraph_gen_;
+	fawkes::Position3DInterface *ifc_pose_;
+
+	ros::Publisher pub_expl_zone_info_;
+	ros::Publisher pub_mps_marker_array_;
+	ros::Publisher pub_mps_light_state_;
+	ros::Publisher pub_pose_;
+	ros::ServiceServer srv_navgraph_gen_;
 };
 
-
-void
-init(ros::NodeHandle &n)
-{
-	if (! blackboard_)  return;
-
-	dp_ = std::make_shared<DataPasser>();
-
-	ifc_zone_ = blackboard_->open_for_reading<fawkes::ZoneInterface>("/explore-zone/info");
-	ifc_tag_vision_ = blackboard_->open_for_reading<fawkes::TagVisionInterface>("/tag-vision/info");
-	for (size_t i = 0; i < std::min(ifc_tag_vision_->maxlenof_tag_id(), ifcs_tag_pos_.size()); ++i) {
-		std::string ifc_id = "/tag-vision/" + std::to_string(i);
-		ifcs_tag_pos_[i] = blackboard_->open_for_reading<fawkes::Position3DInterface>(ifc_id.c_str());
-		// do not listen for events on these interfaces, we read them
-		// whenever the tag vision info interface changes
-	}
-	ifc_machine_signal_ = blackboard_->open_for_reading<fawkes::RobotinoLightInterface>("/machine-signal/best");
-	ifc_navgraph_gen_ =
-		blackboard_->open_for_reading<fawkes::NavGraphWithMPSGeneratorInterface>("/navgraph-generator-mps");
-	ifc_pose_ = blackboard_->open_for_reading<fawkes::Position3DInterface>("Pose");
-	
-	dp_->add(ifc_zone_);
-	dp_->add(ifc_tag_vision_);
-	dp_->add(ifc_machine_signal_);
-	dp_->add(ifc_pose_);
-
-	// Setup ROS topics
-	pub_expl_zone_info_ =
-		n.advertise<rcll_fawkes_sim_msgs::ExplorationZoneInfo>("rcll_sim/explore_zone_info", 10);
-	pub_mps_marker_array_ =
-		n.advertise<rcll_fawkes_sim_msgs::MPSMarkerArray>("rcll_sim/mps_marker_array", 10);
-	pub_mps_light_state_ =
-		n.advertise<rcll_fawkes_sim_msgs::MPSLightState>("rcll_sim/mps_light_state", 10);
-	pub_pose_ =
-		n.advertise<geometry_msgs::PoseWithCovarianceStamped>("rcll_sim/amcl_pose", 10);
-
-	blackboard_->register_listener(dp_.get(), fawkes::BlackBoard::BBIL_FLAG_DATA);
-
-  // provide services
-  srv_navgraph_gen_ = n.advertiseService("rcll_sim/navgraph_generate", srv_cb_navgraph_gen);
-}
 
 int
 main(int argc, char **argv)
@@ -338,27 +352,32 @@ main(int argc, char **argv)
 
 	ros::NodeHandle n;
 
-	// Parameter parsing	
+	std::string  cfg_fawkes_host_;
+	int          cfg_fawkes_port_;
+	std::shared_ptr<fawkes::BlackBoard> blackboard_;
+	std::shared_ptr<RcllFawkesSimNode> node;
+
 	GET_PRIV_PARAM(fawkes_host);
 	GET_PRIV_PARAM(fawkes_port);
-
-	try {
-		blackboard_ =
-			std::make_shared<fawkes::RemoteBlackBoard>(cfg_fawkes_host_.c_str(), cfg_fawkes_port_);
-		init(n);
-	} catch (fawkes::Exception &e) {
-		ROS_WARN("%s: Initial connection request failed, will keep trying", ros::this_node::getName().c_str());
-	}
 
 	while (ros::ok()) {
 		if (!blackboard_) {
 			try {
 				blackboard_ =
 					std::make_shared<fawkes::RemoteBlackBoard>(cfg_fawkes_host_.c_str(), cfg_fawkes_port_);
-				ROS_INFO("%s: Blackboard connected", ros::this_node::getName().c_str());
-				init(n);
-			} catch (fawkes::Exception &e) {}
+				node = std::make_shared<RcllFawkesSimNode>(n, blackboard_);
+				node->init();
+				ROS_INFO("%s: Blackboard connected and initialized", ros::this_node::getName().c_str());
+			} catch (fawkes::Exception &e) {
+				ROS_WARN_THROTTLE(10, "%s: Initialization failed, retrying", ros::this_node::getName().c_str());
+				if (node) {
+					node->finalize();
+					node.reset();
+				}
+				blackboard_.reset();
+			}
 		} else if (! blackboard_->is_alive()) {
+			ROS_WARN_THROTTLE(30, "%s: blackboard connection lost, retrying", ros::this_node::getName().c_str());
 			if (blackboard_->try_aliveness_restore()) {
 				ROS_INFO("%s: Blackboard re-connected", ros::this_node::getName().c_str());
 			}
@@ -367,7 +386,7 @@ main(int argc, char **argv)
 		ros::getGlobalCallbackQueue()->callAvailable(ros::WallDuration(0.1));
 	}
 
-  blackboard_->unregister_listener(dp_.get());
-  
+	if (node) node->finalize();
+
 	return 0;
 }
